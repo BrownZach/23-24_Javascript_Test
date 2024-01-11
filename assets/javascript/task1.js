47800c4fc5931e6f71545a56692e759675c2fb3b
BrownZach <zachary.brown@vtsd.com>
2024-01-11 11:46:17 -0500

    On Task 3

--- assets/javascript/task1.js
+++ assets/javascript/task1.js
@@ -1,1 +1,7 @@
 /* Put code for the task below */
+function task1btnAppear(){
+    document.getElementById("buttonTask1").style.display = "block";
+}
+function task1btnClick(){
+    document.getElementById("imageTask1").style.display = "none"
+}
--- assets/javascript/task2.js
+++ assets/javascript/task2.js
@@ -1,1 +1,10 @@
 /* Put code for the task below */
+function task2redbtnClick(){
+    document.getElementById("task2Table").style.backgroundColor = "red";
+}
+function task2greenbtnClick(){
+    document.getElementById("task2Table").style.backgroundColor = "green";
+}
+function task2bluebtnClick(){
+    document.getElementById("task2Table").style.backgroundColor = "blue";
+}
